baf3a909cd82aba9b01d897a3e924d9f33f2a08f
Илья Емец <suoza089@gmail.com>
2026-01-21 11:36:29 +0300

    Добавьте файлы проекта.

---
+++ src/Scanner.WPF/Options/ScannerOptions.cs
@@ -0,0 +1,27 @@
+﻿namespace Scanner.WPF.Options
+{
+	public class ScannerOptions
+	{
+		/// <summary>
+		/// Известные префиксы
+		/// </summary>
+		/// <remarks>
+		/// Корпус -> Body;
+		/// Монтаж -> Mounting;
+		/// Сила -> Power;
+		/// Управление -> Management;
+		/// Проверка -> Check;
+		/// </remarks>
+		public string[] KnownPrefixes { get; init; } = [];
+
+		/// <summary>
+		/// Интервал проверки порта в секундах
+		/// </summary>
+		public int PortScanIntervalSeconds { get; init; } = 3;
+
+		/// <summary>
+		/// Время ожидания порта в секундах
+		/// </summary>
+		public int PortStaleSeconds { get; init; } = 15;
+	}
+}
